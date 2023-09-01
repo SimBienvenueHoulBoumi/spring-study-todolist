8bde8f603a33c77ecefc67ae0bdc7ff9e98e0865
Sim <brhulla@gmail.com>
2023-09-01 14:26:56 +0200

    initialisation

---
+++ src/main/java/simtodo/SpringStudyTodolistApplication.java
@@ -0,0 +1,13 @@
+package simtodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyTodolistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simtodo/SpringStudyTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package simtodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
